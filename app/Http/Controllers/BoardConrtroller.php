2d3f5e8320a98041bffff6705766c6ddbef31e80
Aleksandr-code <70668106+Aleksandr-code@users.noreply.github.com>
2024-10-11 17:41:15 +0500

    update board controller

--- app/Http/Controllers/BoardConrtroller.php
+++ app/Http/Controllers/BoardConrtroller.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Board;
 use Illuminate\Http\Request;
 
 class BoardConrtroller extends Controller
 {
     public function index(){
-        return response('Сайт объявлений')->header('Content-Type', 'text/plain');
+        $boards = Board::latest()->get();
+        $content = "Объявления\r\n\r\n";
+        foreach ($boards as $board){
+            $content .= $board->title."\r\n";
+            $content .= $board->content."\r\n";
+            $content .= $board->price."руб. \r\n";
+            $content .= "\r\n";
+        }
+        return response($content)->header('Content-Type', 'text/plain');
     }
 }
